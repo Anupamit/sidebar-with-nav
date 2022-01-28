aad164c17c78d6fe9e2d4b0b88a3641f1b8d3e19
Anupam kumar <anupamit011@gmail.com>
2022-01-28 12:10:44 +0530

    Attendence file updated

---
+++ src/pages/Attendence.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Attendence() {
+    return (
+        <div className='attendence'>
+            <h1>Attendence Hai Bhai</h1>
+        </div>
+    );
+}
+
+export default Attendence;
