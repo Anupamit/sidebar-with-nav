53b70a0a443961cb11a1da1cb08794d40e05c8b8
Anupam kumar <anupamit011@gmail.com>
2022-01-28 12:12:08 +0530

    logout page updated

---
+++ src/pages/Logout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Logout() {
+    return (
+        <div className='logout'>
+            <h1>Logout Hai Bhai</h1>
+        </div>
+    );
+}
+
+export default Logout;
