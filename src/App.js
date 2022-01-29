606b089137e8397c543f132d6d950582194a62b2
Anupam kumar <anupamit011@gmail.com>
2022-01-29 12:20:18 +0530

    update to the final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router,Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Notification from './pages/Notification';
+import Attendence from './pages/Attendence';
+import Exam from './pages/Exam';
+import Logout from './pages/Logout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path='/' exactcomponent={Home} />
+          <Route path='/notification' component={Notification} />
+          <Route path='/attendence' component={Attendence} />
+          <Route path='/exam' component={Exam} />
+          <Route path='/logout' component={Logout} />
+        </Routes>
+      </Router>
+    </>
   );
 }
-
 export default App;
